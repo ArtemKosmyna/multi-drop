5964f41b0be024a917a1df06422987f97749d239
Dmytro Chystiakov <dlchistyakov@gmail.com>
2024-04-01 10:53:09 +0300

    prettify code

--- Firmware/Arduino/firmware/firmware.ino
+++ Firmware/Arduino/firmware/firmware.ino
@@ -1,3 +1,5 @@
+// Version 1.0.0
+
 #include <Servo.h>
 
 Servo myservo;
@@ -8,7 +10,6 @@ const int positionStop = 1500;
 const int minMagnet = 514;
 const int maxMagnet = 525;
 
-
 const int hallSensorOpenPin = 2;
 const int hallSensorClosedPin = 1;
 
@@ -18,91 +19,85 @@ bool lastSignalState = false;
 bool closedStateDetected = false;
 bool openStateDetected = false;
 int refilled = 5;
-bool refill_complited = false;
+bool refillCompleted = false;
 
-bool drop_active = false;
+bool dropActive = false;
 
 bool processSignal() {
-  static int thresholdOn = 1900;
-  static int thresholdOff = 1000;
-  int duration = pulseIn(activationPin, HIGH);
+    static int thresholdOn = 1900;
+    static int thresholdOff = 1000;
+    int duration = pulseIn(activationPin, HIGH);
 
-  if (duration > thresholdOn && !lastSignalState) {
-    lastSignalState = true;
-    Serial.println("Rotate");
-    return true;
-  } else if (duration < thresholdOff && lastSignalState) {
-    lastSignalState = false;
-  }
-  return false;
+    if (duration > thresholdOn && !lastSignalState) {
+        lastSignalState = true;
+        Serial.println("Rotate");
+        return true;
+    } else if (duration < thresholdOff && lastSignalState) {
+        lastSignalState = false;
+    }
+    return false;
 }
 
 int readHallSensor(int pin) {
-  return analogRead(pin);
+    return analogRead(pin);
 }
 
 void setup() {
-  myservo.attach(servoPin);
-  pinMode(hallSensorOpenPin, INPUT);
-  pinMode(hallSensorClosedPin, INPUT);
-  Serial.begin(9600);
-  Serial.println("------------------CALIBRATION-------------------");
+    myservo.attach(servoPin);
+    pinMode(hallSensorOpenPin, INPUT);
+    pinMode(hallSensorClosedPin, INPUT);
+    Serial.begin(9600);
+    Serial.println("---------- Multi Drop Version 1.0.0 ------------");
 }
 
 void loop() {
-  int closedStateValue = readHallSensor(hallSensorClosedPin);
-  int openStateValue = readHallSensor(hallSensorOpenPin);
+    int closedStateValue = readHallSensor(hallSensorClosedPin);
+    int openStateValue = readHallSensor(hallSensorOpenPin);
 
-  if (closedStateValue < minMagnet || closedStateValue > maxMagnet) {
-    // Serial.println("detectedClosedState");
-    // Serial.println(closedStateValue);
-    closedStateDetected = true;
-  }
-
-  if (openStateValue < minMagnet || openStateValue > maxMagnet) {
-    // Serial.println("detectedClosedState");
-    // Serial.println(closedStateValue);
-    openStateDetected = true;
-  }
-
-  delay(20); // Delay to prevent excessive servo movement or signal processing
-
-  if (refilled != 0) {
-      if (processSignal() && refilled > 0) {
-        Serial.println("Refill Started");
-        myservo.writeMicroseconds(positionMove);
+    if (closedStateValue < minMagnet || closedStateValue > maxMagnet) {
+        closedStateDetected = true;
     }
 
-    if (refilled != 0 && closedStateDetected && (openStateValue < minMagnet || openStateValue > maxMagnet)) {
-        Serial.println("Refill Waiting");
-        Serial.println(closedStateValue);
-        Serial.println(openStateValue);
-        myservo.writeMicroseconds(positionStop);
-        refilled--;
-        closedStateDetected = false;
+    if (openStateValue < minMagnet || openStateValue > maxMagnet) {
+        openStateDetected = true;
     }
 
-    if (refilled == 0) {
-        Serial.println("Refill Completed");
-        Serial.println(refilled);
-        refill_complited = true;
-    }
-  }
+    delay(20); // Delay to prevent excessive servo movement or signal processing
 
-  if (refill_complited) {
-    if (processSignal()) {
-      Serial.println("Drop started");
-      myservo.writeMicroseconds(positionMove);
-      drop_active = true;
-    }
-    if (openStateDetected && (closedStateValue < minMagnet || closedStateValue > maxMagnet)) {
-        Serial.println("Drop activeted");
-        Serial.println(closedStateValue);
-        Serial.println(openStateValue);
-        myservo.writeMicroseconds(positionStop);
-        openStateDetected = false;
+    if (refilled != 0) {
+        if (processSignal() && refilled > 0) {
+            Serial.println("Refill Started");
+            myservo.writeMicroseconds(positionMove);
+        }
+
+        if (refilled != 0 && closedStateDetected && (openStateValue < minMagnet || openStateValue > maxMagnet)) {
+            Serial.println("Refill Waiting");
+            Serial.println(closedStateValue);
+            Serial.println(openStateValue);
+            myservo.writeMicroseconds(positionStop);
+            refilled--;
+            closedStateDetected = false;
+        }
+
+        if (refilled == 0) {
+            Serial.println("Refill Completed");
+            Serial.println(refilled);
+            refillCompleted = true;
+        }
     }
 
-  }
-
+    if (refillCompleted) {
+        if (processSignal()) {
+            Serial.println("Drop started");
+            myservo.writeMicroseconds(positionMove);
+            dropActive = true;
+        }
+        if (openStateDetected && (closedStateValue < minMagnet || closedStateValue > maxMagnet)) {
+            Serial.println("Drop activated");
+            Serial.println(closedStateValue);
+            Serial.println(openStateValue);
+            myservo.writeMicroseconds(positionStop);
+            openStateDetected = false;
+        }
+    }
 }
